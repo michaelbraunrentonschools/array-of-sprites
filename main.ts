e033c0709cbf4ea6a35bfbca7cf41b8c76f816ea
Michael Braun <57547002+michaelbraunrentonschools@users.noreply.github.com>
2020-02-28 10:51:07 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+// GOAL 1: Create your five favorite foods and save them as INDIVIDUAL VARIABLES using sprite variables
+//         choose food from GALLARY and customize them
+
+// GOAL 2: Store each food as seperate variables 
+
+// GOAL 3: Create an listArray that stores all food in a single array list
+
+// GOAL 4: Display the food on the screen in random locations. 
+
+// GOAL 5: Label each food that is stored in the array. This is a challenge.
+
